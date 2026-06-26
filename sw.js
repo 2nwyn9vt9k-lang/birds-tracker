bd077e305cbe95f6966444cb6dcc6ed5b5397c41
2nwyn9vt9k-lang <2nwyn9vt9k@privaterelay.appleid.com>
2026-06-26 10:45:49 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bird-tracker-v33-1';
+const CACHE = 'bird-tracker-v34';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./index.html'])));
 });
